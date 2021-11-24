60719e61f0a0efa1b4db600428e3ef369a12f0ba
saivivek321 <saivivekkadali@gmail.com>
2021-11-24 21:10:06 +0530

    added more cards

--- index.js
+++ index.js
@@ -5,26 +5,14 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 var categorys=[
-    {
-       "title":"Cycle",
-       "img_add":"imgs/1.jpg"
-    },
-    {
-        "title":"Books",
-        "img_add":"imgs/2.jpg"
-    },
-    {
-        "title":"Drafter",
-        "img_add":"imgs/3.jpg"
-    },
-    {
-        "title":"Chart-Holder",
-        "img_add":"imgs/4.jpg"
-    }
-]
-;
+    {"title":"Cycle","img_add":"imgs/1.jpg"},
+    {"title":"Books","img_add":"imgs/2.jpg"},
+    {"title":"Drafter","img_add":"imgs/3.jpg"},
+    {"title":"Chart-Holder","img_add":"imgs/4.jpg"},
+    {"title":"Calculator","img_add":"imgs/5.jpg"},
+    {"title":"Stationary","img_add":"imgs/6.jpg"}
+];
 
-var category=["Cycle","Books","Drafter","Chart-holder"];
 app.get("/", (req, res) => {
     res.render("buyer",{Categories:categorys});
 })
