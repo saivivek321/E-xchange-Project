a979d9c7f90f0e7aebc5ebad9fb2860ee467f70e
saivivek321 <saivivekkadali@gmail.com>
2021-11-24 21:53:52 +0530

    Created Seller Form

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ app.use(express.static("public"));
 var categorys=[
     {"title":"Cycle","img_add":"imgs/1.jpg"},
     {"title":"Books","img_add":"imgs/2.jpg"},
-    {"title":"Drafter","img_add":"imgs/3.jpg"},
-    {"title":"Chart-Holder","img_add":"imgs/4.jpg"},
     {"title":"Calculator","img_add":"imgs/5.jpg"},
-    {"title":"Stationary","img_add":"imgs/6.jpg"}
+    {"title":"Stationary","img_add":"imgs/6.jpg"},
+    {"title":"Drafter","img_add":"imgs/3.jpg"},
+    {"title":"Chart-Holder","img_add":"imgs/4.jpg"}
 ];
 
+
 app.get("/", (req, res) => {
-    res.render("buyer",{Categories:categorys});
+    res.render("seller",{Categories:categorys});
 })
 app.listen(3000,function(){
     console.log("Server Started and listening on 3000");
