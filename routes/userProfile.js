18bd4a1ec9a73c2cad1da99d0bc236b6b5e78ac3
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-12-01 15:08:13 +0530

    basic setup of mvc model v1

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -5,13 +5,10 @@ const profileController = require('../controllers/profile')
 const router = express.Router();
 
 // /profile/ => GET
-router.get('/profile', profileController.getProfileData);
+router.get('/', profileController.getProfileData);
 // /profile/orders => GET
 router.get('/orders', profileController.getProfileOrders);
 // /profile/sales => GET
 router.get('/sales', profileController.getProfileSales);
-
-// /admin/add-product => POST
-// router.post('/add-product', profileController.postAddProduct);
 
 module.exports = router
